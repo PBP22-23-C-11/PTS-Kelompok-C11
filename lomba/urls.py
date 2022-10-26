a46fec3cae772eff922ea999bb8920065759e38c
Kevin Alexander <kevinalexande26@gmail.com>
2022-10-26 18:29:08 +0700

    Navbar and fixing general

--- lomba/urls.py
+++ lomba/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from lomba.views import show_lomba
+from lomba.views import show_lomba, daftar_lomba, vote_lomba, buat_lomba
 
 app_name = 'lomba'
 
 urlpatterns = [
     path('', show_lomba, name='show_lomba'),
+    path('buat/', buat_lomba, name='buat_lomba'),
+    path('vote/', vote_lomba, name='vote_lomba'),
+    path('daftar/', daftar_lomba, name='daftar_lomba'),
 ]
--- lomba/views.py
+++ lomba/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from general.utils import *
 
 # Create your views here.
 def show_lomba(request):
-    return render(request, "lomba.html")
+    check = check_user_type(request.user)
+    context = {
+        'user': request.user,
+        'check': check,
+    }
+    return render(request, "lomba.html", context)
+
+def buat_lomba(request):
+    return HttpResponse('Welcome Admin')
+
+def daftar_lomba(request):
+    return HttpResponse('Welcome UMKM')
+
+def vote_lomba(request):
+    return HttpResponse('Welcome Customer')
