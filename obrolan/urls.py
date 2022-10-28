30f1ba3171fbd2157863534ef66b1b21f2293d70
mnabielap <mnabielap@gmail.com>
2022-10-28 18:41:41 +0700

    Updating obrolan

--- obrolan/urls.py
+++ obrolan/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from obrolan.views import show_obrolan
+from obrolan.views import create_diskusi_ajax, delete_disc, show_obrolan, show_diskusi_json
 
 app_name = 'obrolan'
 
 urlpatterns = [
     path('', show_obrolan, name='show_obrolan'),
+    path('add/', create_diskusi_ajax, name='create_diskusi_ajax'),
+    path('json/', show_diskusi_json, name='show_diskusi_json'),
+    path('delete-disc/<int:id>/', delete_disc, name='delete-disc'),
 ]
