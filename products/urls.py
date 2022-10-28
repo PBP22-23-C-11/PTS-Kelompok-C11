710fead81ff26c3441419e889642803d5a0e2a6b
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-10-28 17:06:13 +0700

    Update models, views, urls, and forms

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products.views import show_products
+from products.views import add_product
 
 app_name = 'products'
 
 urlpatterns = [
     path('', show_products, name='show_products'),
+    path('add/', add_product, name='add_product'),
 ]
