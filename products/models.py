4ef5492ab8a901e2299e82a659abb27d3e1ead0a
ahmadhanif3 <ahmad.hanif11@ui.ac.id>
2022-11-02 00:33:45 +0700

    Changed product

--- products/models.py
+++ products/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.conf import settings
-from general.models import UMKM
 from django.contrib.auth.models import User
+from general.models import UMKM
 
 # Create your models here
 class Product(models.Model):
-    UMKM_name = models.CharField(max_length=255)
-    product_name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) # Satu UMKM bisa memiliki banyak product
+    UMKM_name = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = models.TextField()
-    owner = models.ForeignKey(UMKM, on_delete=models.CASCADE) # Satu UMKM bisa memiliki banyak product
+    description = models.TextField(max_length=500)
