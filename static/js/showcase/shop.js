8bad8d610a618d83b2631a7ec47fe27fcdda5910
ahmadhanif3 <ahmad.hanif11@ui.ac.id>
2022-11-02 22:43:48 +0700

    Fixed external js

--- static/js/showcase/shop.js
+++ static/js/showcase/shop.js
@@ -1,8 +1,10 @@
 const doneButton = document.getElementById("rate-shop-submit");
-const rateButton = document.getElementById("btnRate")
+const rateButton = document.getElementById("btnRate");
+const forCsrf = document.getElementById("for-csrf").value;
+const forPk = document.getElementById("for-pk").value;
 
 doneButton.onclick = function() {
     const rating_total = $("#rating_total").val()
-    const data = {rating_total:rating_total, csrfmiddlewaretoken:"{{ csrf_token }}"}
-    $.ajax({url:"/showcase/rate_shop/{{ umkm.pk }}", data:data, method:"POST"})
+    const data = {rating_total:rating_total, csrfmiddlewaretoken:forCsrf}
+    $.ajax({url:`/showcase/rate_shop/${forPk}`, data:data, method:"POST"})
 }
--- static/js/showcase/showcase.js
+++ static/js/showcase/showcase.js
@@ -2,23 +2,32 @@ const doneButton = document.getElementById("add-shop-submit");
 const addButton = document.getElementById("btnAdd");
 
 doneButton.onclick = function() {
-    const shop_name = $("#shop_name").val()
-    const category = $("#category").val()
-    const desc = $("#description").val()
-    const umkm_url = $("#umkm_url").val()
-    const number = $("#number").val()
-    const image = $("#image").val()
-    const data =  {shop_name:shop_name, category:category, description:desc, umkm_url:umkm_url, number:number, image:image, csrfmiddlewaretoken:"{{ csrf_token }}"}
-    $.ajax({url:"/showcase/add_shop/", data:data, method:"POST"}).done(function() {
-        show_cards()
-        addButton.style.display = "none"
-    })
+    const shop_name = $("#shop_name").val();
+    const category = $("#category").val();
+    const desc = $("#description").val();
+    const umkm_url = $("#umkm_url").val();
+    const number = $("#number").val();
+    const image = $("#image").val();
+    const forCsrf = document.getElementById("for-csrf").value;
+    const data =  {shop_name:shop_name, 
+                    category:category, 
+                    description:desc, 
+                    umkm_url:umkm_url, 
+                    number:number, 
+                    image:image, 
+                    csrfmiddlewaretoken:forCsrf};
+    $.ajax({url:"/showcase/add_shop/", 
+            data:data, 
+            method:"POST"}).done(function() {
+        show_cards();
+        addButton.style.display = "none";
+    });
 }
 
 
 function show_cards() {
     $.get('/showcase/json', function(shops) {
-        $('#myCards').empty()
+        $('#myCards').empty();
         shops.map((shop) => {
             $('#myCards').append(`
                 <div class="col-xs-12 col-sm-4">
@@ -32,11 +41,11 @@ function show_cards() {
                         <a class="card-link" href="/showcase/shop/${shop.pk}"></a>
                     </div>
                 </div>
-            `)
-        })
+            `);
+        });
     })
 }
 
 $(document).ready(function() {
-    show_cards()
+    show_cards();
 })  
