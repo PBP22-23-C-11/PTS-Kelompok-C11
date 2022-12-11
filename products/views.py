2c32e1cd969104d5cb3da8d2151663d79dfcb5b7
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-12-11 13:47:41 +0700

    Add views for flutter

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponseBadRequest, HttpResponse, JsonResponse
@@ -40,6 +41,34 @@ def add_product(request):
             return JsonResponse(products)
         return HttpResponseBadRequest()
     
+@csrf_exempt
+def add_product_flutter(request):
+    if request.method == 'POST':
+        person = request.POST.get("UMKM")
+        temp = User.objects.get(username=person)
+
+        form = AddProductForm(request.POST)
+        if form.is_valid():
+            UMKM_name = form.cleaned_data['UMKM_name']
+            product_name = form.cleaned_data['product_name']
+            price = form.cleaned_data['price']
+            description = form.cleaned_data['description']
+            owner = temp
+                   
+            product = Product.objects.create(UMKM_name=UMKM_name, product_name=product_name, price=price, description=description, owner=owner)
+
+            products = {
+                'pk' : product.pk,
+                'fields' : {
+                    'UMKM_name' : product.UMKM_name,
+                    'product_name' : product.product_name,
+                    'price' : product.price,
+                    'description' : product.description,
+                }
+            }
+            return JsonResponse(products)
+        return HttpResponseBadRequest()
+
 def show_json(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
