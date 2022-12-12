4b4f0571fd49e5b83a88d73da98a7aaaad565f72
ahmadhanif3 <ahmad.hanif11@ui.ac.id>
2022-12-12 17:01:10 +0700

    Views for POST

--- showcase/urls.py
+++ showcase/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', show_showcase, name='show_showcase'),
     path('shop/<int:id>', show_shop, name="show_shop"),
     path('json/', show_json, name="show_json"),
+
     path('add_shop/', add_shop, name="add_shop"),
+    path('add_shop_flutter/', add_shop_flutter, name="add_shop_flutter"),
+    
     path('rate_shop/<int:id>', rate_shop, name="rate_shop"),
 ]
